7af55b00716f042e9039a556bbf8118059d721c3
ueki5 <ueki005@gmail.com>
2025-03-30 07:14:11 +0000

    繰り返し、リスト処理

--- src/Study/St006.hs
+++ src/Study/St006.hs
@@ -8,21 +8,30 @@ import Data.String.Interpolate
 fizzbuzz:: Int -> String
 fizzbuzz n | n `mod` 3 == 0 && 
              n `mod` 5 == 0 = [i|#{n} fizzbuzz|]
-fizzbuzz n | n `mod` 3 == 0 = "fizz"
-fizzbuzz n | n `mod` 5 == 0 = "buzz"
-fizzbuzz _ | otherwise = "何者でもない"
+fizzbuzz n | n `mod` 3 == 0 = [i|#{n} fizz|]
+fizzbuzz n | n `mod` 5 == 0 = [i|#{n} buzz|]
+fizzbuzz n | otherwise = [i|#{n} どちらでもない|]
 
 -- 関数定義
-exec :: String -> IO () -> IO ()
-exec str act = do
+execr :: String -> IO () -> IO ()
+execr str act = do
+  putStrLn str
+  act
+execl :: IO () -> String -> IO ()
+execl act str = do
   putStrLn str
   act
 -- 処理実行
 sub006 :: IO ()
--- sub006 = do
---   putStrLn "処理実行"
-  -- putStrLn $ "No1:" ++ fizzbuzz 3
-  -- putStrLn $ "No2:" ++ fizzbuzz 5
-  -- putStrLn $ "No3:" ++ fizzbuzz 15
-  -- putStrLn $ "No4:" ++ fizzbuzz 1
-sub006 = foldr exec (return ()) (map fizzbuzz [1 .. 20])
+sub006 = do
+  putStrLn "処理実行"
+  putStrLn "<<fold right"
+  foldr 
+    execr 
+    (putStrLn "fold right>>")
+    (map fizzbuzz [1 .. 20])
+  putStrLn "<<fold left"
+  foldl
+    execl
+    (putStrLn "fold left>>")
+    (map fizzbuzz [1 .. 20])
